adeae7e7a5205dfdeffdaefd7f08684ec24b3eaa
pixelhn_r5s <pixelhn@qq.com>
2022-07-29 12:53:34 +0000

    clear

---
+++ re_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+work_path=$(pwd)
+main_path=$work_path/sdk/src/project
+build_path=$work_path/sdk/build
+bin_file=$work_path/out.bin
+k210tty=/dev/ttyUSB0
+
+if [ ! -f $main_path/dev.h ]; then
+    cp $project_path/dev.h $main_path
+fi
+
+if [ ! -f $main_path/dev.c ]; then
+    cp $project_path/dev.c $main_path
+fi
+rm $work_path/end.bin
+
+    echo _________________BUILD_ALL!__________________
+    cd $work_path/sdk/build
+    rm -rf *
+    cmake .. -DPROJ=project -DTOOLCHAIN=/mnt/sdcard/K210/kendryte-toolchain/bin || exit
+    make -j4 || exit
+    echo _________________ALL_RIGHT!__________________
--- up_build.sh
+++ up_build.sh
@@ -19,5 +19,5 @@ make -j4 || exit
 rm $bin_file
 mv $work_path/sdk/build/project.bin $bin_file
 rm project*
-/home/rock/.local/bin/kflash -p $k210tty $bin_file -b 1500000 -B maixduino -t
+kflash -p $k210tty $bin_file -b 1500000 -B maixduino -t
 
